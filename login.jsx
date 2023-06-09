e6c7cc759543aef30f1029f52f01586a82a1316a
Nurul Farada <128475676+nurulfarada@users.noreply.github.com>
2023-06-09 23:54:29 +0700

    Add files via upload

--- login.jsx
+++ login.jsx
@@ -1,40 +1,79 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import BgSection from '../components/bgComponent'
-import InputRegister from '../components/inputComponent'
-import google from '../assets/icons/Google.png'
-import facebook from '../assets/icons/facebook.png'
-import "../assets/css/style.css";
-
+import React from "react"
 export default function Login() {
-  return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-      <BgSection />
-      <div className='bg-purple-sosialify flex flex-col justify-center'>
-        <form className='max-w-[400px] w-full mx-auto rounded-lg bg-purple-sosialify p-1 px-8' action=''>
-          <h2 className='text-4xl text-yellow-500 font-bold text-center'>Selamat Datang</h2>
-          <p className='text-white text-center mt-4'>Belum memiliki akun? <NavLink to="/register" className='text-red-500'>Daftar Sekarang</NavLink></p>
-          <InputRegister text='Email' type='email' placeholder='Masukkan email Anda' />
-          <InputRegister text='Password' type='password' placeholder='Masukkan password Anda' />
-
-          <div className='flex justify-end text-gray-400 py-2'>
-            <NavLink to="/forgot-password" className='text-xs'>Lupa kata sandi?</NavLink>
+    return (
+      <>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-cyan-900">
+              Login in to your account
+            </h2>
           </div>
-          <button className='w-full my-5 py-2 bg-yellow-500 text-black shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 font-semibold rounded-lg'>Masuk</button>
-
-        </form>
-
-        <div className="flex justify-center mb-5">
-          <p className='text-white font-bold relative line'>Atau</p>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </label>
+                  <div className="text-sm">
+                    <a href="#" className="font-semibold text-teal-800 hover:text-indigo-500">
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-teal-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm text-indigo-950">
+              Don't have an a account?{' '}
+              <a href="#" className="font-semibold leading-6 text-gr hover:text-pink-950">
+                Sign up
+              </a>
+            </p>
+          </div>
         </div>
-
-        <div className="flex flex-col flex-wrap content-center gap-3">
-          <button className='bg-white rounded-xl py-2 px-8 btn-auth'><NavLink to="#" className='flex justify-between'> <img className='w-6 mr-4 ml-14' src={google} alt="" /> <span className="mr-auto">Masuk dengan Google</span></NavLink></button>
-          <button className='bg-white rounded-xl py-2 px-8 btn-auth'><NavLink to="#" className='flex justify-between'> <img className='w-6 mr-4 ml-14' src={facebook} alt="" /> <span className="mr-auto">Masuk dengan Facebook</span></NavLink></button>
-        </div>
-
-        <NavLink to="#" className='text-orange-300 ml-16 mt-16'>Kembali ke beranda</NavLink>
-      </div>
-    </div>
-  )
-}              
+      </>
+    );
+  }
+  
